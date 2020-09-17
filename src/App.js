32e794ebda334eb9b789b7d04fd0b22ff9a9df43
salEZY <bobicaleks@yahoo.com>
2020-09-17 13:21:14 +0200

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 
 import Main from "./components/Main/Main";
 
 function App() {
+  const Header = styled.header`
+    background-color: #282c34;
+    height: 8vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+
+  const Footer = styled.footer`
+    background-color: #282c34;
+    height: 8vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <h1>Traversal</h1>
-      </header>
+      </Header>
       <Main />
-      <footer className="App-footer">
+      <Footer>
         <p>bobicaleks@yahoo.com</p>
-      </footer>
+      </Footer>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-
-import "./Main.css";
+import styled from "styled-components";
 
 const Main = () => {
   const [start, setStart] = useState(false);
@@ -13,16 +12,36 @@ const Main = () => {
     setStart(false);
   };
 
+  const Wrapper = styled.main`
+    min-height: 84vh;
+    text-align: center;
+  `;
+  const Button = styled.button`
+    margin-top: 50px;
+    width: 30%;
+    padding: 1rem;
+    font-size: 2rem;
+    font-weight: bolder;
+    color: tomato;
+    background: #282c34;
+    border: 0;
+    outline: 0;
+    border: 5px solid tomato;
+    border-radius: 5px 10px 50px 5px;
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  `;
+
   return (
-    <main>
+    <Wrapper>
       {start ? (
         <p onClick={stopHandler}>BLOCKS</p>
       ) : (
-        <button className="start-button" onClick={startHandler}>
+        <Button className="start-button" onClick={startHandler}>
           START
-        </button>
+        </Button>
       )}
-    </main>
+    </Wrapper>
   );
 };
 
